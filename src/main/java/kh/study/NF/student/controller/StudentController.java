f2f3823c44529b5c352257d6cffb3eada41a4e20
kskdeveloper <sukyung5555@gmail.com>
2022-11-01 21:23:24 +0900

    학생의 복학, 휴학, 전과신청 페이지 및 복학, 휴학, 전과신청, 복수전공 신청현황을 볼 수 있는 페이지 -수경

--- src/main/java/kh/study/NF/student/controller/StudentController.java
+++ src/main/java/kh/study/NF/student/controller/StudentController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/stu")
 public class StudentController {
-	//by수경 학생정보시스템의 첫페이지이자 로그인 페이지입니다.
+	//by수경 학생정보시스템의 첫페이지 로그인 페이지입니다.
 	@GetMapping("/main")
 	public String stuMain() {
 		return "content/student/stuMain";
@@ -17,6 +17,20 @@ public class StudentController {
 	@GetMapping("/changeMajor")
 	public String changeMajor() {
 		return "content/student/changeMajor";
+	}
+	
+	//by수경 학생이 학교를 휴학신청하는 페이지로 이동
+	@GetMapping("/takeOffUniv")
+	public String takeOffUniv() {
+		
+		return  "content/student/takeOffUniv";
+	}
+	
+	//by수경 학생이 학교를 복학신청하는 페이지로 이동
+	@GetMapping("/returnUniv")
+	public String returnUniv() {
+		
+		return  "content/student/returnUniv";
 	}
 	
 	//by수경 학생의 전공학과 변경(전과), 휴학신청, 복학신청, 복수전공(전공 하나 더) 신청 현황을 모아 둔 페이지
