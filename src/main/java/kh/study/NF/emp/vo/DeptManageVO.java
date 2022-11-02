fc3cc1b894d89e37889571e10bad3990b8439f94
kskdeveloper <sukyung5555@gmail.com>
2022-11-02 12:39:05 +0900

    DeptManage mapper, DeptManageVO - 수경

---
+++ src/main/java/kh/study/NF/emp/vo/DeptManageVO.java
@@ -0,0 +1,28 @@
+package kh.study.NF.emp.vo;
+
+import groovy.transform.ToString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@ToString
+public class DeptManageVO {
+	//by수경 Dept_Manage 테이블 기반으로 만든 VO입니다.
+	String applyNo;
+	String stuNo;
+	String applyCode;
+	String applyDate;
+	String applyReason;
+	String approvalDate;
+	String processStatus;
+	String fromColl;
+	String fromDept;
+	String toColl;
+	String toDept;
+	String doubleMajorColl;
+	String doubleMajorDept;
+	
+	
+	
+}
