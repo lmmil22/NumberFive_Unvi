a655ecf5a2c978263dcd21671072fb37eb35730a
kskdeveloper <sukyung5555@gmail.com>
2022-11-02 14:14:50 +0900

    colleageVO, deptManage mapper수정 - 수경

---
+++ src/main/java/kh/study/NF/dept/vo/ColleageVO.java
@@ -0,0 +1,15 @@
+package kh.study.NF.dept.vo;
+
+import groovy.transform.ToString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@ToString
+public class ColleageVO {
+	String collNo;
+	String collName;
+	
+	
+}
