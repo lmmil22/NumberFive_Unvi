27b39011556fc3bde93c9c5c04858719eac23345
lmmil22 <lmmil22ill@gmail.com>
2022-11-02 16:44:50 +0900

    CSS 폴더와 강의 DEPT목록 셀렉트박스까지 -지아

--- src/main/resources/static/js/regproFLecture.js
+++ src/main/resources/static/js/regproFLecture.js
@@ -5,11 +5,6 @@ function changeDept(){
  	const collNo = document.querySelector('select').value;
 			
  	alert(collNo);
-//const tbody = document.querySelector('.stuListTable > tbody');
-			//const deptSelect = document.querySelector('.changeDeptList');
-			//const optionTags = deptSelect.querySelectorAll('option');
-			//deptSelect.removeChild(optionTags); // 부모 테이블 중에 자식을 지울건데 티바디만 지울거에요
-			
 	 //ajax start
 	 $.ajax({
 		 url: '/proF/getDeptListAjax', 
@@ -17,46 +12,24 @@ function changeDept(){
 		 data: {'collNo':collNo},
 		 success: function(result) {
 			
-			//현재 (지금) 열러있는 html파일에서 찾는다 (클래스가stuListTable을 찾는다  )
-			//const deptSelect = document.querySelector('.changeDeptList');
+			const deptSelect = document.querySelector('.changeDeptList');
 			
-			//전체가 뜨는 테이블은 지워준다 
 			
-			//const tbody = document.querySelector('.stuListTable > tbody');
-			//const optionTags = deptSelect.querySelectorAll('option');
-			//***** 이걸 사용하면 FOR 문을 돌려서 하나씩 지워줘야한다 자식들을  */deptSelect.removeChild(optionTags); // 부모 테이블 중에 자식을 지울건데 티바디만 지울거에요
-			$('.changeDeptList').empty(); //제이 쿼리문법인데 FOR문을 돌리지않고 자식들을 다 지워줄 수 있다 
+			const optionTags = deptSelect.querySelectorAll('option');
+			$('.changeDeptList').empty(); //모든 자식들 지워줄거에요 
 		
-		//추가할 태그를 문자열로 받음 (+= 를 사용ㅇ하면 문자열이 계속 연결된다 )
 			let str ='';
 		
 
-			str += '<option>';
-			for(const dept of result){ //학생목록이 다 있다 . stu = 학생한명
-
-
-//최근버전 백틱사용 
-
-
-			str += '<tr>';
-			str += `<td>${stu.stuNum}</td>`;
-		/*	str += `<td>${stu.stuName}</td>`;*/
-			
-			str += `<td>`;
-		/*	str += `<span onclick="showDetail();">${stu.stuName}</span>`;*/
-			str += `<a onclick="showDetail(${stu.stuNum});">${stu.stuName}</a>`;
-			str += `</td>`;
-			
-			str += `<td>${stu.stuAge}</td>`;
-			str += `<td>${stu.className}</td>`;
-			str += `<td><input type="button" value="삭제" onclick="deleteStu(${stu.stuNum},this);">               </td>`;
-			str += '</tr>';
-
-			}
-
-			str += '</tbody>';
 		
-		stuListTable.insertAdjacentHTML('beforeend',str); //이 테이블이 끝나기 전에 str을 붙여주겠다 
+			for(const dept of result){ //옵션이 계속 반복된다
+			str += `<option value="${dept.deptNo}">${dept.deptName}</option>`;
+				}			
+
+
+
+		
+		deptSelect.insertAdjacentHTML('beforeend',str); // 셀렉트박스가 끝나기 전에 str을 붙여주겠다 
 		
 			
 		
