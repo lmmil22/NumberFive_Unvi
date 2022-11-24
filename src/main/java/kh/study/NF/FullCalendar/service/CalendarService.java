d47b437da6768df14f000b489d3085da4a32d606
lmmil22 <lmmil22ill@gmail.com>
2022-11-24 14:56:59 +0900

    fullcalendar 학사일정 DB조회 mapper 작성 , service조회된 결과값 map 사용해서 list로 리턴 --지아

--- src/main/java/kh/study/NF/FullCalendar/service/CalendarService.java
+++ src/main/java/kh/study/NF/FullCalendar/service/CalendarService.java
@@ -3,8 +3,11 @@ package kh.study.NF.FullCalendar.service;
 import java.util.List;
 import java.util.Map;
 
+import kh.study.NF.FullCalendar.vo.CalendarVO;
+
 public interface CalendarService {
 
 	 List<Map<String, Object>> getEventList();
-	
+	List<CalendarVO> selectRegDate();
+	 
 }
--- src/main/java/kh/study/NF/FullCalendar/service/CalendarServiceImpl.java
+++ src/main/java/kh/study/NF/FullCalendar/service/CalendarServiceImpl.java
@@ -5,34 +5,68 @@ import java.util.Map;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
-
-
+import java.util.Iterator;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import kh.study.NF.FullCalendar.vo.CalendarVO;
+//by 지아 캘린더에 사용되는 service입니다
 @Service("CalendarService")
 public class CalendarServiceImpl implements CalendarService {
 
 	@Autowired
 	private SqlSessionTemplate sqlSession;
-	
-	
 
+	//by 지아 DB에 등록된 일정 조회
+	@Override
+	public List<CalendarVO> selectRegDate() {
+		return sqlSession.selectList("calendarMapper.selectRegDate");
+
+	}
+
+	//by 지아 map에 조회된 일정 넣고 리턴
 	@Override
 	public List<Map<String, Object>> getEventList() {
-		 Map<String, Object> event = new HashMap<String, Object>();
-	        List<Map<String, Object>> eventList = new ArrayList<Map<String, Object>>();
-	        event.put("start", LocalDate.now());
-	        event.put("title", "test");
-	        event.put("end",LocalDate.now());
-	        eventList.add(event);
-	        event = new HashMap<String, Object>();
-	        event.put("start", LocalDate.now().plusDays(3));
-	        event.put("title", "test2");
-	        event.put("end",LocalDate.now().plusDays(4));
-	        eventList.add(event);
-	        return eventList;
+
+		List<Map<String, Object>> eventList = new ArrayList<Map<String, Object>>();
+
+		List<CalendarVO> events = selectRegDate();
+
+		// 필요한 시간양식 '2020-11-77'
+		Map<String, Object> event = new HashMap<String, Object>();
+//
+//		event.put("start", calendarVO.getRegdate());
+//		event.put("title", "받아온 데이터");
+//		event.put("end", LocalDate.now());
+//		eventList.add(event);
+		
+		 for(int i =0;  i < events.size(); i++ ) {
+			
+				event = new HashMap<String, Object>();
+				
+				event.put("start", events.get(i).getStartdate());
+				
+				//System.out.println("!!!!!!!!!!!!!!!"+events.get(i).getRegdate());
+				event.put("title", events.get(i).getTitle());
+				event.put("color", events.get(i).getCalColor());
+				
+				if (events.get(i).getEnddate() != null) {
+					event.put("end", events.get(i).getEnddate());
+					
+				}
+				
+				//if (end 데이터값이 없으면 데이터 값 안들어가게 만들어주기
+				eventList.add(event);
+					 
+			 
+		 }
+			System.out.println(eventList);
+
+		// 캘린더에 넘겨주는 리스트
+		return eventList;
+
 	}
+
 }
