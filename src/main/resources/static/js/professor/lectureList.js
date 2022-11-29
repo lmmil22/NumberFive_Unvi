6142be23ad548669a7330f29c1305b9d3777dd87
lmmil22 <lmmil22ill@gmail.com>
2022-11-29 15:49:27 +0900

    베이스랑 탑 레이아웃 원래대로 리셋,강의 리스트 제이쿼리 추가 --지아

--- src/main/resources/static/js/professor/lectureList.js
+++ src/main/resources/static/js/professor/lectureList.js
@@ -1,4 +1,9 @@
 //by 지아 
+//
+ $('a').click(function(event){
+    event.preventDefault(); 
+  });
+
 //페이징 라이브러리
 var box = paginator({
     table: document.getElementById("table_box_bootstrap").getElementsByTagName("table")[0],
