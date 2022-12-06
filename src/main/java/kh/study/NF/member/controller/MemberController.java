948f58c7d21b93b26c488a5acab116aaf645b3a7
AmVinch <ub997@naver.com>
2022-12-06 21:30:50 +0900

    로그인 후 알림창 swal로 바꾸기 수정진행중 -by유빈

--- src/main/java/kh/study/NF/member/controller/MemberController.java
+++ src/main/java/kh/study/NF/member/controller/MemberController.java
@@ -46,46 +46,7 @@ public class MemberController {
    @Autowired
    private PasswordEncoder encoder;
 //-------------------------------------------------------------------------------------------///   
-   
-   //[홈화면 모달창] 회원가입
-   @PostMapping("/join")
-   public String join(@Valid MemberVO memberVO, BindingResult bindingResult, Model model) {
-      // !! validation 체크 (데이터 유효성 검증)
-      if (bindingResult.hasErrors()) {// 바인딩하는데 오류가 생겼니?: 결과는 true/false
-         model.addAttribute("memberVO", memberVO);//회원가입 실패시 입력 데이터 값 유지.
-         System.out.println("@@@@@@@@@@@@@@@@@@@@ 회원가입 유효성체크 >>> error발생   @@@@@@@@@@@@@@@@@@@");
-         System.out.println("지금 바인딩 오류의 상태는? " + bindingResult.hasErrors());//true
-         //어떤 오류인지 확인
-          List<ObjectError> list =  bindingResult.getAllErrors();
-             for(ObjectError e : list) {
-                  System.out.println(e.getDefaultMessage());
-             }
-      }
-      // 기본값을 student로 주기.
-      memberVO.setMemRole(MemRole.STUDENT.toString());
-      
-      //암호화 작업2 -주석풀기 
-      memberVO.setMemPw(encoder.encode(memberVO.getMemPw()));
-      
-      //회원가입 
-      memberService.join(memberVO);
-      
-      return "content/common/join_result";
-   }
-//-------------------------------------------------------------------------------------------///   
-   // by 유빈 : 학생정보시스템의 첫 화면 로그인 페이지입니다.
-   // --> stu컨트롤러에서 학생,교수,교직원 모두 로그인해야해서 공통사항은 (common폴더)여기로 옮겼어!!)
-   // 첫 화면 경로 : http://localhost:8081/member/homeLogin
-   @GetMapping("/homeLoginTest")
-   public String homeLogin(MemberVO memberVO, boolean isLoginFail, Model model ) {
-      //-----로그인 성공 및 실패 여부를 html에 데이터 전달하기-------//
-      System.out.println("____________지금 로그인 실패니???_________" + isLoginFail);
-      model.addAttribute("isLoginFail",isLoginFail);
-      return "content/common/home_Login_Test"; //by 유빈 :로그인페이지는 공통이라 common폴더 아래 login으로 파일 만들었어!!
-   }
-   
-//---------------------------------------------------------------------------------------------//   
-   // !!!!! 실제 !!!!! 11/27 이미지 꽉차게 테스트용 경로
+   // 실제 홈화면(시큐리티 사용)
    @GetMapping("/homeLogin")
    public String homeLoginTest(MemberVO memberVO, boolean isLoginFail, Model model,BoardVO boardVO,String boardNo,SearchVO searchVO ) {
 			System.out.println("SearchKeyword=" + boardVO.getSearchKeyword());
@@ -98,11 +59,42 @@ public class MemberController {
 			model.addAttribute("boardList",boardService.selectBoardList(boardVO));
 			System.out.println("_________________게시판 목록 조회 성공_______________");
 			
-      return "content/common/home_Login"; //by 유빈 :로그인페이지는 공통이라 common폴더 아래 login으로 파일 만들었어!!
-      // 언니 수정부분 return "content/common/home_Login_test"; //by 유빈 :로그인페이지는 공통이라 common폴더 아래 login으로 파일 만들었어!!
+      return "content/common/home_Login"; 
+      //by 유빈 :로그인페이지는 공통이라 common폴더 아래 login으로 파일 만들었음
+      // 언니 수정부분 return "content/common/home_Login_test"; //by 유빈 :로그인페이지는 공통이라 common폴더 아래 login으로 파일 만들었음
       //by수경 최종 사용을 위하여 페이지 이름 변경 및 페이지 경로 수정(11/30)
    }
-   
+ //---------------------------------------------------------------------------------------------//   
+   // 미사용 [홈화면 모달창] 회원가입
+	/*
+	 * @PostMapping("/join") public String join(@Valid MemberVO memberVO,
+	 * BindingResult bindingResult, Model model) { // !! validation 체크 (데이터 유효성 검증)
+	 * if (bindingResult.hasErrors()) {// 바인딩하는데 오류가 생겼니?: 결과는 true/false
+	 * model.addAttribute("memberVO", memberVO);//회원가입 실패시 입력 데이터 값 유지. System.out.
+	 * println("@@@@@@@@@@@@@@@@@@@@ 회원가입 유효성체크 >>> error발생   @@@@@@@@@@@@@@@@@@@");
+	 * System.out.println("지금 바인딩 오류의 상태는? " + bindingResult.hasErrors());//true
+	 * //어떤 오류인지 확인 List<ObjectError> list = bindingResult.getAllErrors();
+	 * for(ObjectError e : list) { System.out.println(e.getDefaultMessage()); } } //
+	 * 기본값을 student로 주기. memberVO.setMemRole(MemRole.STUDENT.toString());
+	 * 
+	 * //암호화 작업2 -주석풀기 memberVO.setMemPw(encoder.encode(memberVO.getMemPw()));
+	 * 
+	 * //회원가입 memberService.join(memberVO);
+	 * 
+	 * return "content/common/join_result"; }
+	 */
+//-------------------------------------------------------------------------------------------///   
+   // by 유빈 : 학생정보시스템의 첫 화면 로그인 페이지입니다.
+   // --> stu컨트롤러에서 학생,교수,교직원 모두 로그인해야해서 공통사항은 (common폴더)여기로 옮겼어!!)
+   // 첫 화면 경로 : http://localhost:8081/member/homeLogin
+	/* 미사용 페이지????
+	 * @GetMapping("/homeLoginTest") public String homeLogin(MemberVO memberVO,
+	 * boolean isLoginFail, Model model ) { //-----로그인 성공 및 실패 여부를 html에 데이터
+	 * 전달하기-------// System.out.println("____________지금 로그인 실패니???_________" +
+	 * isLoginFail); model.addAttribute("isLoginFail",isLoginFail); return
+	 * "content/common/home_Login_Test"; //by 유빈 :로그인페이지는 공통이라 common폴더 아래 login으로
+	 * 파일 만들었어!! }
+	 */
    
 //-------------------------------------------------------------------------------------------///   
    // 로그인성공시,로그인실패시 -> 로그인페이지로 이동
@@ -138,18 +130,16 @@ public class MemberController {
       System.out.println("  ________________  현재 조회된 정보가 있나?__________   " + loginInfo);//null
       return "redirect:/member/homeLogin";//login상태가 null일때, alert으로 올바르지않은 이메일과 학번/교번이라 뜨면서 다시 입력하세요라고한다ㅏ
    }
-//-------------------------------------------------------------------------------------------///   
-
-   
-   
-   
    
 //-------------------------------------------------------------------------------------------///   
+   // 비밀번호변경 페이지 (임시발급 비밀번호로 로그인후)
    @GetMapping("/updatePw")
    public String updatePwForm() {
 	   System.out.println("비밀번호변경 페이지 이동 성공!!!!");
 	   return "content/common/update_pw";
    }    
+//-------------------------------------------------------------------------------------------///   
+   //실제 비밀번호 변경 페이지 
    @PostMapping("/updatePw")
    public String updatePw(MemberVO memberVO, Authentication authentication) {
 	   System.out.println("이메일로 발급된 임시비밀번호에서 다시 비밀번호수정해서 변경시키면 form태그로 넘어오기 성공!!!!");
@@ -165,7 +155,6 @@ public class MemberController {
 	   return "redirect:/member/homeLogin";
    }    
 //-------------------------------------------------------------------------------------------///   
-   
    // ajax로 이메일 임시비밀번호 발급 후 이동 페이지 ???
    @PostMapping("/afterLogin")
    public String afterLogin1(boolean isLoginFail, Model model) {
@@ -176,7 +165,6 @@ public class MemberController {
       return "redirect:/member/homeLogin";
    }
 //-------------------------------------------------------------------------------------------///   
-
    // 로그인 후 첫 화면 > 다시 홈화면
    @GetMapping("/afterLogin")
    public String afterLogin(boolean isLoginFail, Model model) {
@@ -208,29 +196,28 @@ public class MemberController {
       return "content/common/afterLogin";
    }
 //-------------------------------------------------------------------------------------------//   
-   // 나의 정보관리 클릭시,
-   @GetMapping("/myinfo")
-   public String myinfo(String memNo, Model model,Authentication authentication) {
-      //시큐리티 사용했으므로 로그인한 유저의 정보를 가져온다.
-      User user = (User)authentication.getPrincipal();
-      //로그인한 사람의 아이디,비밀번호,권한정보 데이터만 가져올수있다.
-      System.out.println("id = " + user.getUsername());
-      //System.out.println("pw = " + encoder.encode(user.getPassword()));
-      
-      //회원상세정보조회
-      model.addAttribute("memInfo", memberService.selectMemberDetail(memNo));
-      
-      return "content/common/my_info";
-   }
+   //미사용..? 탑메뉴에서 나의 정보관리 클릭시,
+	/*
+	 * @GetMapping("/myinfo") public String myinfo(String memNo, Model
+	 * model,Authentication authentication) { //시큐리티 사용했으므로 로그인한 유저의 정보를 가져온다. User
+	 * user = (User)authentication.getPrincipal(); //로그인한 사람의 아이디,비밀번호,권한정보 데이터만
+	 * 가져올수있다. System.out.println("id = " + user.getUsername());
+	 * //System.out.println("pw = " + encoder.encode(user.getPassword()));
+	 * 
+	 * //회원상세정보조회 model.addAttribute("memInfo",
+	 * memberService.selectMemberDetail(memNo));
+	 * 
+	 * return "content/common/my_info"; }
+	 */
 //-------------------------------------------------------------------------------------------//
 
-   //상세조회 후 회원정보수정
-   @PostMapping("/updateMemInfo")
-   public String updateMemInfo() {
-      return "redirect:/member/myInfo";
-   }
+   // 미사용 상세조회 후 회원정보수정
+	/*
+	 * @PostMapping("/updateMemInfo") public String updateMemInfo() { return
+	 * "redirect:/member/myInfo"; }
+	 */
+/////////////////////////////////// [ 관리자 모드 ] //////////////////////////////////////////////////////
 //-------------------------------------------------------------------------------------------//
-   
    // [관리자모드] - 회원등록양식페이지이동
    @GetMapping("/regMemForm")
    public String regMemForm(MemberVO memberVO, boolean isLoginFail, Model model ) {
@@ -276,6 +263,7 @@ public class MemberController {
       return "content/common/after_Reg_Mem";
    }
 //-------------------------------------------------------------------------------------------//
+   // (미사용)탑메뉴클릭시, '학교소개'에서 나오는 페이지로 사용??
    @GetMapping("/intro")
    public String intro() {
       return "content/common/intro";
